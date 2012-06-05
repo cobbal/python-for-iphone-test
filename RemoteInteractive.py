50fcb1d9c2ca628c58d7e5d3ccde2d70f0e452d7
Andrew Cobb <andrew.cobb@gmail.com>
2012-06-04 22:08:16 -0600

    now chooses a socket automatically

--- RemoteInteractive.py
+++ RemoteInteractive.py
@@ -1,12 +1,8 @@
 import code, socket, sys
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-for port in range(5000, 5010):
-    try:
-        server_socket.bind(('', 5000))
-        break
-    except socket.error:
-        pass
+server_socket.bind(('', 0))
+print "listening for connection on on %s:%s" % server_socket.getsockname()
 server_socket.listen(1)
 client_socket, address = server_socket.accept()
 print 'I got a connection from ', address
